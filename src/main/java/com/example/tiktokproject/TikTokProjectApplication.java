12fd65f48850c2b884feafbd598c469898608c45
Mario Metushev <mariometushev1@gmail.com>
2022-02-11 21:20:12 +0200

    Mario added Spring project

---
+++ src/main/java/com/example/tiktokproject/TikTokProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.tiktokproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TikTokProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TikTokProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tiktokproject/TikTokProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tiktokproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TikTokProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
