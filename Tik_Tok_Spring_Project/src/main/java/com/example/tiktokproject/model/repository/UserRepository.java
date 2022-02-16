52212352b4da8c3a7fc78834785c0223d5ee9ed0
tomienk <tomienk@abv.bg>
2022-02-16 22:53:22 +0200

    Merge the changes

--- Tik_Tok_Spring_Project/src/main/java/com/example/tiktokproject/model/repository/UserRepository.java
+++ Tik_Tok_Spring_Project/src/main/java/com/example/tiktokproject/model/repository/UserRepository.java
@@ -2,12 +2,15 @@ package com.example.tiktokproject.model.repository;
 
 import com.example.tiktokproject.model.pojo.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+
 
     Optional<User> findByPhoneNumber(String phone_number);
 
